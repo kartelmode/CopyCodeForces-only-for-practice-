ebdc31f12bb797bd3b43dd32cb4e52626c9b9ef7
kartelmode <65723695+kartelmode@users.noreply.github.com>
2022-06-24 12:34:42 +0300

    Add files via upload

---
+++ src/App/HeaderAndBox/HeaderAndBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContestBox from './ContestBox/ContestBox.jsx'
+import Header from './Header/Header.jsx'
+
+export default function HeaderAndBox() {
+  return (
+    <div>
+        <Header/>
+        <ContestBox/>
+    </div>
+  )
+}
